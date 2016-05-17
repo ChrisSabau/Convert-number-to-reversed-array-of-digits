e1fa432a6bafd6db8e473a54a373aae2b8635a31
Christian Sabau <csabau@rewardsnetwork.com>
2016-05-17 08:26:22 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# Convert-number-to-reversed-array-of-digits" 
+Given a random integer, return the integers within an array in reverse order.
+
+Example:
+
+348597 => [7,9,5,8,4,3]
--- src/DigitizeNumbers.java
+++ src/DigitizeNumbers.java
@@ -1,9 +1,5 @@
 public class DigitizeNumbers {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-
     public static int[] digitize(long n) {
         String[] ints = new StringBuilder(Long.toString(n)).reverse().toString().split("");
         int[] numsToArray = new int[ints.length];
